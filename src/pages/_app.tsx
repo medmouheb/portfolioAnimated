f5842ae756091a34ef93272bc5a5b7b93e7c5949
medmouheb <mohamedmouheb@gmail.com>
2024-07-02 10:06:44 +0100

    mouheb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import "@/styles/globals.scss";
 import { appWithTranslation } from 'next-i18next';
+import Head from 'next/head';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>My Next.js App</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default appWithTranslation(MyApp);
